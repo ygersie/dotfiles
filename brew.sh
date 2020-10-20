2ae707bd1e458527e38bf0902dba78f2e78a8823
Yorick Gersie <ygersie@ebay.com>
2020-10-20 11:13:44 +0200

    include google chrome

--- brew.sh
+++ brew.sh
@@ -97,6 +97,7 @@ brew install mas # command line interface for the Mac App store
 
 brew cask install slack
 brew cask install docker
+brew cask install google-chrome
 brew cask install visual-studio-code
 brew cask install google-cloud-sdk
 
